70ed9a01a58dda2bc7815ca73b42a93c31e9f79a
Alessia Monasteri <alessia.monasteri99@gmail.com>
2025-04-11 15:34:55 +0200

    3.Car.java

--- Car.java
+++ Car.java
@@ -6,12 +6,23 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("Hello world from a Car class");
         Car car1 = new Car ();
-        Car car2 = new Car ();
-        Car car3 = new Car ();
+        car1.brand = "Tesla";
+        car1.model = "Model S";
+        car1.year = 2024;
 
-        System.err.println("Car 1: " + car1.brand + "" + car1.model + "" + car1.year);
-        System.err.println("Car 2: " + car2.brand + "" + car2.model + "" + car2.year);
-        System.err.println("Car 3: " + car3.brand + "" + car3.model + "" + car3.year);
+        Car car2 = new Car ();
+        car2.brand = "Ford";
+        car2.model = "Focus";
+        car2.year = 2018;
+
+        Car car3 = new Car ();
+        car3.brand = "Tesla";
+        car3.model = "Model X";
+        car3.year = 2023;
+
+        System.err.println("Car 1: " + car1.brand + " " + car1.model + " " + car1.year);
+        System.err.println("Car 2: " + car2.brand + " " + car2.model + " " + car2.year);
+        System.err.println("Car 3: " + car3.brand + " " + car3.model + " " + car3.year);
     }
 
 }
