851f7dcf76049eaaab7c474ee6d24f8010d1d23a
Alessia Monasteri <alessia.monasteri99@gmail.com>
2025-04-11 13:41:57 +0200

    Car.java commit

---
+++ Car.java
@@ -0,0 +1,9 @@
+public public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from a Car class");
+    }
+}
