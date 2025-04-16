0220a1c2428172b7393f8085a17771c43d70a769
Alessia Monasteri <alessia.monasteri99@gmail.com>
2025-04-16 12:57:00 +0200

    1.Movie

---
+++ Movie/generics/DVDPlayer.java
@@ -0,0 +1,15 @@
+package Movie.generics;
+public class DVDPlayer {
+ public void on() {
+ System.out.println("DVD player is on");
+ }
+ public void play(String movie) {
+ System.out.println("Playing movie: " + movie);
+ }
+ public void stop() {
+ System.out.println("Stopping DVD player");
+ }
+ public void off() {
+ System.out.println("DVD player is off");
+ }
+}
---
+++ Movie/generics/Projector.java
@@ -0,0 +1,13 @@
+package Movie.generics;
+public class Projector {
+ public void on() {
+ System.out.println("Projector is on");
+ }
+ public void wideScreenMode() {
+ System.out.println(
+ "Projector in widescreen mode");
+ }
+ public void off() {
+ System.out.println("Projector is off");
+ }
+}
